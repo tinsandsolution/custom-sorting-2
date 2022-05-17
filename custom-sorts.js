965f5721c7b56bb1b887829ebe2ad237c8e50191
Mariesa <mariesa.oconnell@gmail.com>
2022-05-17 16:19:08 -0400

    second completed

--- custom-sorts.js
+++ custom-sorts.js
@@ -5,7 +5,16 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    if(a % 2 === 1 && b % 2 === 0){ // if we have an odd & an even, do line 9
+      return -1; // moves a to the right
+    }
+    if (a % 2 === 0 && b % 2 === 1){
+      return 1; // moves/keeps a to the left
+    }
+    return a - b;
+  })
+
 }
 
 function validAnagrams(s, t) {
